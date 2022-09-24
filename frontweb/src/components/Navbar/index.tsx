27f65b644d6a9add4d1593fc63a91855e9fb4435
Thiago Gonçalves <mg.az.ul@hotmail.com>
2022-09-24 14:58:31 -0300

    Navbar hamburger menu

--- frontweb/src/components/Navbar/index.tsx
+++ frontweb/src/components/Navbar/index.tsx
@@ -1,15 +1,30 @@
 import './styles.css';
+import 'bootstrap/js/src/collapse.js';
 
 const Navbar = () => {
   return (
     <>
-      <nav className="navbar navbar-expand-md bg-primary main-nav">
+      <nav className="navbar navbar-expand-md navbar-dark bg-primary main-nav">
 
         <div className="container-fluid">
-          <a href="link" className="nav-log-text ">Carros Top</a>
+          <a href="link" className="nav-log-text ">
+            <h2>Carros Top</h2>
+          </a>
+
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#cartop-navbar"
+            aria-controls="cartop-navbar"
+            aria-expanded="false"
+            aria-label="Toggle navigation">
+
+            <span className="navbar-toggler-icon"></span>
+          </button>
         </div>
 
-        <div className="collapse navbar-collapse">
+        <div className="collapse navbar-collapse" id="cartop-navbar">
           <ul className="navbar-nav main-menu" >
             <li>
               <a href="link" className="active">Home</a>
