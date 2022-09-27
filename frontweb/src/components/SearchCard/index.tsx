1df8a1c85a6d4e7ee736033e2de41c7cfe554096
Thiago Gonçalves <mg.az.ul@hotmail.com>
2022-09-27 12:20:58 -0300

    SearchCard

---
+++ frontweb/src/components/SearchCard/index.tsx
@@ -0,0 +1,22 @@
+import "./styles.css";
+
+
+const SearchCard = () => {
+  return (
+    <>
+      <div className="search-card-container">
+        <form action="" className="search-form">
+          <div>
+          <label htmlFor="ctrl-search"></label>
+          <input className="input-box" id="ctrl-search" name="search" placeholder="Digite sua busca" type="text" />
+          </div>
+          <div className="btn-container">
+          <input type="submit" value="Buscar" className="btn btn-secondary"/>
+          </div>
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default SearchCard;
