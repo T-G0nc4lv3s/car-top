dfd28ab26cf3183678a4976b646b01de5aefb494
Thiago Gonçalves <mg.az.ul@hotmail.com>
2022-09-27 12:24:30 -0300

    Page Catalog

---
+++ frontweb/src/pages/Catalog/index.tsx
@@ -0,0 +1,19 @@
+import './styles.css';
+import Navbar from 'components/Navbar';
+import SearchCard from 'components/SearchCard';
+import ProductCard from 'components/ProductCard';
+
+const Catalog = () => {
+  return (
+    <>
+      <Navbar />
+      <div className="catalog-container">
+      <SearchCard />
+      <ProductCard />
+      </div>
+      
+    </>
+  );
+};
+
+export default Catalog;
