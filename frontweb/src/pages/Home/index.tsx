3c7242fa1fd8a94e03b2472b1b6e8340951394e2
Thiago Gonçalves <mg.az.ul@hotmail.com>
2022-09-29 20:44:43 -0300

    Home refactor

--- frontweb/src/pages/Home/index.tsx
+++ frontweb/src/pages/Home/index.tsx
@@ -1,28 +1,28 @@
 import './styles.css';
 import MainImg from 'assets/images/main-img.svg';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
-    <div className="home-container container-fluid">
+    <div className="home-container">
       <div className="card-top-container">
-        <div className="top-image-container">
+        <div className="image-container">
           <img src={MainImg} alt="Imagem principal" />
         </div>
-
-        <div className="top-header-container">
-          <h1>O carro perfeito para você</h1>
-        </div>
-
-        <div className="top-content-container">
-          <p>
-            Conheça nossos carros e dê mais um passo na realização do seu sonho
-          </p>
+        <div>
+          <div className="header-container">
+            <h1>O carro perfeito para você</h1>
+          </div>
+          <div className="content-container">
+            <p>
+              Conheça nossos carros e dê mais um passo na realização do seu
+              sonho
+            </p>
+          </div>
         </div>
       </div>
-
       <div className="card-bottom-container">
-        <button className="btn btn-secondary btn-icon"><Link to="/products">VER CATÁLOGO</Link></button>
+        <button className="btn btn-secondary"><Link to="/products">VER CATÁLOGO</Link></button>
         <span>Comece agora a navegar</span>
       </div>
     </div>
