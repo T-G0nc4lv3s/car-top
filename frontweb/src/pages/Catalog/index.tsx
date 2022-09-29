29c032f897046490344421c4b6d342917066f299
Thiago Gonçalves <mg.az.ul@hotmail.com>
2022-09-29 12:21:42 -0300

    Catalog grid system

--- frontweb/src/pages/Catalog/index.tsx
+++ frontweb/src/pages/Catalog/index.tsx
@@ -9,6 +9,26 @@ const Catalog = () => {
       <Navbar />
       <div className="catalog-container">
         <SearchCard />
+        <div className="row">
+          <div className="col-sm-6 col-lg-4">
+            <ProductCard />
+          </div>
+          <div className="col-sm-6 col-lg-4">
+            <ProductCard />
+          </div>
+          <div className="col-sm-6 col-lg-4">
+            <ProductCard />
+          </div>
+          <div className="col-sm-6 col-lg-4">
+            <ProductCard />
+          </div>
+          <div className="col-sm-6 col-lg-4">
+            <ProductCard />
+          </div>
+          <div className="col-sm-6 col-lg-4">
+            <ProductCard />
+          </div>
+        </div>
       </div>
     </>
   );
